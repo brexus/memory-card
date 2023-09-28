f5e1ac37b50a837d34c44f9af9cf4423c79dd6de
brexus <92671269+brexus@users.noreply.github.com>
2023-09-28 13:39:39 +0200

    change logical of game over

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,59 +8,74 @@ export default function Card({
     title,
     score,
     setScore,
+    bestScore,
+    setBestScore,
     setIsGameOver,
     mixBoard,
-    isLoading,
-    isMixing,
-    setIsMixing,
-    numberOfCards
+    isFrontSide,
+    setIsFrontSide,
+    numberOfCards,
+    setIsWin
 }) {
 
     const [isCardClicked, setIsCardClicked] = useState(false);
 
     useEffect(() => {
+
+        // Karta ma status klikniętej, 
         if(isCardClicked) {
             setScore(s => s + 1);
-            
+
+            // Kliknięto już wszystkie karty poprawnie === wygrana
             if(score + 1 === numberOfCards) {
+                setIsWin(true);
                 setIsGameOver(true);
+                
+            // Gramy dalej
             } else {
                 mixBoard();
+                setTimeout(() => {
+                    setIsFrontSide(true);
+                }, 1000);
             }
-            setIsMixing(true);
-            setTimeout(() => {setIsMixing(false)}, 1000);
-            
         }
 
     }, [isCardClicked]);
 
-    
+
     return (
         <div 
             className="card"
+
             onClick={() => {
+                setIsFrontSide(false);
+                
+                // Karta nie była kliknięta, więc dostaje status klikniętej
                 if(!isCardClicked) {
                     setIsCardClicked(true);
+
+                // Karta już była kliknięta === przegrana
                 } else {
                     setIsGameOver(true);
+                    setIsWin(false);
                 }
             }}
 
             style={{
-                transform: isMixing ? "rotateY(180deg)" : "",
-                transition: isMixing ? "0.3s" : "0.3s",
+                transform: isFrontSide ? "rotateY(0)" : "rotateY(180deg)",
+                transition: "ease 1s",
             }}
         >
             <img 
                 src={imgURL} 
                 alt="" 
                 className="card-img"
-                style={{display: isMixing ? "none" : "flex"}}
+                style={{display: isFrontSide ? "flex" : "none"}}
             />
 
             <h1 
                 className="card-title"
-                style={{display: isMixing ? "none" : "flex"}}
+                style={{display: isFrontSide ? "flex" : "none"}}
             >
                 {title}
             </h1>
@@ -69,7 +84,7 @@ export default function Card({
                 src="pokeball.png" 
                 style={{
                     width: "100px",
-                    display: isMixing ? "flex" : "none",
+                    display: isFrontSide ? "none" : "flex",
                     justifyContent: "center",
                     alignItems: "center"
                 }}
